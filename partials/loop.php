0668c39d7cfe4f9c87133e1c8aaa20487a3b5c93
Phil Thompson <hello@philthompson.co.uk>
2016-12-21 15:43:05 +0000

    Replaced `screen-reader-text` class with `visuallyhidden`

--- partials/loop.php
+++ partials/loop.php
@@ -69,6 +69,6 @@
 		the_posts_pagination( array(
 			'prev_text'          => 'Previous page',
 			'next_text'          => 'Next page',
-			'before_page_number' => '<span class="meta-nav screen-reader-text">Page </span>',
+			'before_page_number' => '<span class="visuallyhidden">Page </span>',
 		) );
 ?>
